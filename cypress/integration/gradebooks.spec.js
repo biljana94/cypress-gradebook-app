7fa6a21c014fed9cab1ef0d25dfcecbeb660ef80
Jelaca <bilja.jelaca@gmail.com>
2020-12-29 23:08:50 +0100

    filter - isnt finish

--- cypress/integration/gradebooks.spec.js
+++ cypress/integration/gradebooks.spec.js
@@ -8,13 +8,19 @@ let userData = {
     password: 'testtest123'
 }
 
+let filterData = {
+    uppercaseWord: 'DNEVNIK',
+    arrResultUppercase: []
+}
+
 describe('Gradebooks homepage', () => {
     beforeEach('Login', () => {
         cy.visit('/')
-        cy.get(locators.header.loginButton)
-        cy.get(locators.login.email).type(userData.email)
-        cy.get(locators.login.password).type(userData.password)
-        cy.get(locators.login.buttonSubmit).click()
+        // cy.get(locators.header.loginButton)
+        // cy.get(locators.login.email).type(userData.email)
+        // cy.get(locators.login.password).type(userData.password)
+        // cy.get(locators.login.buttonSubmit).click()
+        cy.loginCommand(userData.email, userData.password)
     })
 
     it('Homepage visibility', () => {
@@ -27,10 +33,17 @@ describe('Gradebooks homepage', () => {
                 return expect(dataLength).to.equal(10)
             }
         })
-        // cy.intercept('GET', 'https://gradebook-api.vivifyideas.com/api/diaries?page=1', { fixture: 'stubDiaries.json' })
-        // cy.wait(2000)
-        // cy.visit('/')
     })
 
-    it('Homepagefilter', () => {})
+
+    //Kao korisnik na vrhu home stranice vidim input polje preko kojeg mogu da filtriram dnevnike. 
+    //Kada ukucam termin i kliknem na dugme “Filtriraj” koje se nalazi pored, 
+    //prikazuju mi se samo dnevnici koji imaju ukucan termin u imenu dnevnika (podrazumeva se partial term kao i case insensitive). 
+    //Paginacija se i dalje prikazuje i klikom na “load more” dugme se učitava novih 10 dnevnika koji zadovoljavaju kriterijume filtera.
+
+    it('Homepagefilter - case sensitive test', () => {
+        cy.visit('/')
+        cy.get(locators.homePageGradebooks.inputFilter).should('be.visible').type(filterData.uppercaseWord)
+        cy.get(locators.homePageGradebooks.buttonSearch).eq(0).should('be.visible').click()
+    })
 })

4b1194204f5cafd0046d85a12ed5dbf98b00d960
Jelaca <bilja.jelaca@gmail.com>
2020-12-29 16:47:03 +0100

    myGradebookPage test FINISH

--- cypress/integration/myGradebookPage.spec.js
+++ cypress/integration/myGradebookPage.spec.js
@@ -14,7 +14,10 @@ let data = {
     email: 'test@example.com',
     password: 'testtest123',
     randomImage: faker.image.avatar(),
-    userId: ''
+    userId: '',
+    userFirstName: '',
+    userLastName: '',
+    singleGradebookId: ''
 }
 
 describe('My Gradebook', () => {
@@ -26,12 +29,52 @@ describe('My Gradebook', () => {
 
         }).as('successfulLogin')
         cy.loginCommand(data.email, data.password) // logujemo se u aplikaciju
-        // cekamo da se uradi login i uzimamo response. Iz responsa vadimo userId
+        // cekamo da se uradi login i uzimamo response. Iz responsa vadimo userId, userFirstName, userLastName
         cy.wait('@successfulLogin').then((response) => {
-            console.log(response.response.body.user.id)
+            // console.log(response.response.body.user.id)
             data.userId = response.response.body.user.id
+            data.userFirstName = response.response.body.user.firstName
+            data.userLastName = response.response.body.user.lastName
         })
     })
 
-    it('Gradebook visibility', () => {})
+    it('Gradebook visibility', () => {
+        cy.intercept('GET', `https://gradebook-api.vivifyideas.com/api/diaries/my-diary/${data.userId}`, (req) => {
+
+        }).as('successfulGetMyGradebook')
+        cy.get(locators.header.myGradebook).eq(1).should('be.visible').click()
+        cy.wait('@successfulGetMyGradebook').then((response) => {
+            // console.log(response.response.body.id)
+            data.singleGradebookId = response.response.body.id
+        })
+        cy.url().should('contains', `/my-gradebook/${data.userId}`)
+        cy.get(locators.myGradebook.pageTitle).should('be.visible').and('have.text', 'My Gradebook Page')
+        cy.get(locators.myGradebook.gradebookTitle).should('be.visible').and('have.text', 'test')
+        cy.get(locators.myGradebook.professorName).should('be.visible').and('contain.text', data.userFirstName + ' ' + data.userLastName)
+        cy.get(locators.myGradebook.studentsList).should('be.visible').then(($list) => {
+            console.log($list)
+            if(!$list === '') {
+                return expect($list).to.contain('You do not have an assigned student')
+            } else {
+                return $list
+            }
+        })
+        cy.get(locators.myGradebook.addNewStudent.addStudentButton).eq(0).should('be.visible').click()
+        cy.url().should('contains', `/my-gradebook/add-student/${data.singleGradebookId}`)
+    })
+
+    it('Add New Student', () => {
+        cy.get(locators.header.myGradebook).eq(1).should('be.visible').click()
+        cy.url().should('contains', `/my-gradebook/${data.userId}`)
+        cy.wait(2000)
+        cy.get(locators.myGradebook.gradebookTitle).then(($diary) => {
+            if($diary === '') {
+                cy.get(locators.myGradebook.addNewStudent.addStudentButton).eq(0).should('be.visible').click()
+                return cy.get(locators.myGradebook.addNewStudent.noDiaryValidation).should('be.visible')
+            } else {
+                cy.get(locators.myGradebook.addNewStudent.addStudentButton).eq(0).should('be.visible').click()
+                return cy.url().should('contains', `/my-gradebook/add-student/${data.singleGradebookId}`)
+            }
+        })
+    })
 })
